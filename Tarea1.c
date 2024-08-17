ef13f111643a70f04ce9b8e350fee0d3e2114e38
Guillermo Barquinero <82953082+Guillermo-Adrian-Barquinero@users.noreply.github.com>
2024-08-17 19:04:29 -0300

    Tarea fura de fecha :(

---
+++ Tarea1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    // Cuenta regresiva 10 al 0
+    for (int i = 10; i >= 0; i--) 
+    {
+        printf("%d\n", i);
+    }
+    
+    // Desplega "Hola Mundo"
+    printf("Hola Mundo\n");
+
+    return 0;
+}
